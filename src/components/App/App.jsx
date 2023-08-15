28602aae45417b0b5866ef97c2cd5947ad0d8447
Maksym Triteinikov <i.max.trit@gmail.com>
2023-08-15 16:36:16 +0300

    hw 04 feedback is done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,30 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Statistics } from '../Statistics/Statistics';
 import { FeedbackOptions } from '../Feedback/FeedbackOptions';
 import { Layout } from 'components/Layout';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addFeedback = event => {
+    switch (event.target.name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        setBad(prevState => prevState + 1);
+    }
   };
 
-  addFeedback = event => {
-    this.setState(prevState => {
-      return { [event.target.name]: prevState[event.target.name] + 1 };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const arrOfValues = Object.values(this.state);
+  const countTotalFeedback = () => {
+    const arrOfValues = [good, neutral, bad];
     const total = arrOfValues.reduce(
       (prevValue, value) => prevValue + value,
       0
@@ -27,35 +32,31 @@ export class App extends Component {
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    if (!this.state.good) {
+  const countPositiveFeedbackPercentage = () => {
+    if (!good) {
       return 0;
     }
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <Layout>
-        <GlobalStyle />
-        <FeedbackOptions
-          options={this.state}
-          onLeaveFeedback={this.addFeedback}
+  return (
+    <Layout>
+      <GlobalStyle />
+      <FeedbackOptions
+        options={{ good, neutral, bad }}
+        onLeaveFeedback={addFeedback}
+      />
+      {countTotalFeedback() ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage}
         />
-        {this.countTotalFeedback() ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </Layout>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </Layout>
+  );
+};
